516c9d546f7885c957b6d8c2decff362b2b57264
Mahsabeigi33 <Mahsabeigi33@gmail.com>
2024-01-06 06:43:17 +0330

    reset

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -19,6 +20,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'last_name'
         'email',
         'password',
     ];
--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+    // Revoke all tokens...
+$request->user->tokens()->delete();
+
+// Revoke the token that was used to authenticate the current request...
+$request->request->user()->currentAccessToken()->delete();
+
+// Revoke a specific token...
+$request->user->tokens()->where('id', $tokenId)->delete();
     return $request->user();
+
+});
+Route::post('/tokens/create', function (Request $request) {
+$token = $request->user()->createToken($request->token_name);
+
+return ['token' => $token->plainTextToken];
 });
